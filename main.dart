2964e50959896418d05e381b3e47133422ccadf5
Syed Ameenuddin <ameen.syed124@gmail.com>
2021-11-27 23:30:30 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: new ThemeData(
+        splashColor: Colors.transparent,
+        focusColor: Colors.transparent,
+      ),
+      debugShowCheckedModeBanner: false,
+      title: "Make Decision",
+      home: Scaffold(
+      // backgroundColor: Color(0xFFd7d7d7),
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int num=0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: RaisedButton(
+          highlightColor: Colors.transparent,
+          child: Image.asset('images/ball$num.png',),
+          onPressed: (){
+            setState(() {
+              num=Random().nextInt(8)+1;
+            },);
+          },
+        ),
+   );
+  }
+}
